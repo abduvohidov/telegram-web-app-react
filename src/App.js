7e18cbd999ade574bb4197fef207501cd4fa187f
Abdulloh <abduvohidov.abdullokh@gmail.com>
2023-12-18 22:19:29 +0500

    pr

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      work
-      <button onClick={onClose}>Закрыть</button>
+      <Header />
     </div>
   );
 }
